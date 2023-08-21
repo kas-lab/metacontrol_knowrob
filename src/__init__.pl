7054579f7ecd9e91906b4c9c5a785d90752b9230
Gustavo <g.rezendesilva@tudelft.nl>
2023-08-21 16:43:43 +0200

    ✨ load knowrob, tomasys and metacontrol_knowrob

---
+++ src/__init__.pl
@@ -0,0 +1,3 @@
+:- register_ros_package(knowrob).
+:- load_owl('https://raw.githubusercontent.com/meta-control/mc_mdl_tomasys/ros2/owl/tomasys.owl', [namespace(tomasys, 'http://metacontrol.org/tomasys#')]).
+:- register_ros_package(metacontrol_knowrob).
