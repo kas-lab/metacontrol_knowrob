fe4d20a475be1388410767e1a249d50c47d8192f
Gustavo <g.rezendesilva@tudelft.nl>
2023-08-25 11:15:58 +0200

    ✨ CompErrFg rule

--- src/analyze.pl
+++ src/analyze.pl
@@ -1,17 +1,28 @@
 :- module(analyze,
     [
+      component/1,
+      c_status/2,
+      fd/1,
       fg/1,
-      fg_solves_obj/2,
+      fg_status/2,
+      % fg_solves_obj/2,
+      qa_type/2,
       objective_status/2
     ]).
 
+component(C) :- instance_of(C, tomasys:'Component').
+c_status(C, S) :- triple(C, tomasys:'c_status', S).
+c_required_by(C, FD) :- triple(C, mros:'requiredBy', FD).
+
+fd(FD) :- instance_of(FD, tomasys:'FunctionDesign').
 fg(FG) :- instance_of(FG, tomasys:'FunctionGrounding').
+
 fg_solves_obj(FG, O) :- triple(FG, tomasys:'solvesO', O).
-fg_status(FG, S) :- triple(FG, tomasys:'fg_status', S).
+
 % fg_measured_qa(FG, MQA) :- triple(FG, tomasys:'hasQAvalue', MQA).
-% fg_type(FG, FD) :- triple(FG, tomasys:'typeFD', FD).
+fg_type(FG, FD) :- triple(FG, tomasys:'typeFD', FD).
 % fd_estimated_qa(FD, EQA) :- triple(FD, tomasys:'hasQAestimation', EQA).
-% qa_type(QA, T) :- triple(QA, tomasys:'isQAtype', T).
+qa_type(QA, T) :- triple(QA, tomasys:'isQAtype', T).
 % qa_value(QA, QAV) :- triple(QA, tomasys:'hasValue', QAV).
 
 % fg_status(FG, "IN_ERROR_NFR") :-
@@ -21,3 +32,8 @@ fg_status(FG, S) :- triple(FG, tomasys:'fg_status', S).
 
 % S1a,b,c
 objective_status(O, S) :- fg(FG), fg_solves_obj(FG, O), fg_status(FG, S).
+
+% CompErrFg
+fg_status(FG, 'IN_ERROR_COMPONENT') :- component(C), c_status(C, 'FALSE'),
+  c_required_by(C, FD), fg_type(FG, FD).
+fg_status(FG, S) :- triple(FG, tomasys:'fg_status', S).
