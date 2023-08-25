2db3fdb81a732edd38443bca7e11c11418e2fd4d
Gustavo <g.rezendesilva@tudelft.nl>
2023-08-25 11:08:15 +0200

    ✨ load mros ontology

--- src/__init__.pl
+++ src/__init__.pl
@@ -1,5 +1,6 @@
 :- register_ros_package(knowrob).
 :- load_owl('https://raw.githubusercontent.com/meta-control/mc_mdl_tomasys/ros2/owl/tomasys.owl', [namespace(tomasys, 'http://metacontrol.org/tomasys#')]).
+:- load_owl('https://raw.githubusercontent.com/meta-control/mros_ontology/main/owl/mros.owl', [namespace(mros, 'http://ros/mros#')]).
 :- register_ros_package(metacontrol_knowrob).
 
 :- use_module(library('analyze')).
