17bb610058f024953fb5fba58676b2f54c17c68b
Gustavo <g.rezendesilva@tudelft.nl>
2023-08-26 11:57:07 +0200

    ✨ rules for fg_status Ok & fd_realisability true

--- src/analyze.pl
+++ src/analyze.pl
@@ -47,46 +47,75 @@ qa_has_value(QA, QAV) :- triple(QA, tomasys:'hasValue', QAV).
 % Get property
 qa_comparison_operator(QAT, OP) :-
   qa_type(QAT), triple(QAT, tomasys:'qa_comparison_operator', OP).
+
+% Check if QA is critical
 qa_critical(QAT, CR) :-
   qa_type(QAT), triple(QAT, tomasys:'qa_critical', CR).
+qa_critical(QAT, false) :-
+  qa_type(QAT), \+triple(QAT, tomasys:'qa_critical', _).
 
 c_status(C, S) :- component(C), triple(C, tomasys:'c_status', S).
 
-% S1a,b,c
+%% Objective status (S1a,b,c)
 objective_status(O, S) :- objective(O), triple(O, tomasys:'o_status', S).
-objective_status(O, S) :- fg_solves_obj(FG, O), fg_status(FG, S).
+objective_status(O, S) :-
+  objective(O), \+triple(O, tomasys:'o_status', S),
+  fg_solves_obj(FG, O), fg_status(FG, S).
 % TODO: infer objective UNGROUNDED
 % TODO: infer objective OK/IN_PROGRESS
 % TODO: infer objective status when objective has a required QA
 
 
-% helper predicate to get fg error type
-qa_error_type(QAT, ERROR) :-
-  qa_critical(QAT, true) -> ERROR = 'IN_ERROR_FR'; ERROR = 'IN_ERROR_NFR'.
-% qa_error_type(QAT, 'IN_ERROR_NFR') :- qa_critical(QAT, false).
-
+%% Helper predicates for FG status
 % Compare the measured QA with the estimated QA given the comparison_operator
-qa_comparer(QAT, MV, EV) :-
+qa_violation(QAT, MV, EV) :-
   qa_comparison_operator(QAT, '<'), !, MV < EV;
   qa_comparison_operator(QAT, '<='), !, MV =< EV;
   qa_comparison_operator(QAT, '>'), !, MV > EV;
   qa_comparison_operator(QAT, '>='), !, MV >= EV;
   MV > EV.
 
-
-% CompErrFg
-fg_status(FG, S) :- fg(FG), triple(FG, tomasys:'fg_status', S).
-fg_status(FG, 'IN_ERROR_COMPONENT') :-
-  fg_type(FG, FD), c_required_by(C, FD), c_status(C, 'FALSE').
-
-fg_status(FG, ERROR) :-
+fg_measured_qa_violation(FG, QAT) :-
   fg_measured_qa(FG, MQA), fg_type(FG, FD), fd_estimated_qa(FD, EQA),
   qa_is_type(MQA, QAT), qa_is_type(EQA, QAT), qa_has_value(MQA, MV),
-  qa_has_value(EQA, EV), qa_comparer(QAT, MV, EV), qa_error_type(QAT, ERROR).
-%TODO: infer fg status OK
-% fg_status(FG, 'OK') :- fg(FG).
+  qa_has_value(EQA, EV), qa_violation(QAT, MV, EV).
 
-% CompErr
+%% FG status
+fg_status(FG, S) :- fg(FG), triple(FG, tomasys:'fg_status', S).
+
+fg_status(FG, 'IN_ERROR_COMPONENT') :-
+  fg(FG),
+  \+triple(FG, tomasys:'fg_status', _), %why this doesnt work?
+  fg_type(FG, FD), c_required_by(C, FD), c_status(C, 'FALSE').
+
+fg_status(FG, 'IN_ERROR_FR') :-
+  fg(FG),
+  \+triple(FG, tomasys:'fg_status', _),
+  \+fg_status(FG, 'IN_ERROR_COMPONENT'),
+  fg_measured_qa_violation(FG, QAT), qa_critical(QAT, true).
+
+fg_status(FG, 'IN_ERROR_NFR') :-
+  fg(FG),
+  \+triple(FG, tomasys:'fg_status', _),
+  \+fg_status(FG, 'IN_ERROR_COMPONENT'),
+  \+fg_status(FG, 'IN_ERROR_FR'),
+  fg_measured_qa_violation(FG, QAT), qa_critical(QAT, false).
+
+fg_status(FG, 'OK') :-
+  fg(FG),
+  \+triple(FG, tomasys:'fg_status', _),
+  \+fg_status(FG, 'IN_ERROR_COMPONENT'),
+  \+fg_status(FG, 'IN_ERROR_FR'),
+  \+fg_status(FG, 'IN_ERROR_NFR'),
+  \+fg_measured_qa_violation(FG,_).
+
+% FD Realisability (CompErr)
 fd_realisability(FD, S):- fd(FD), triple(FD, tomasys:'fd_realisability', S).
-fd_realisability(FD, false) :- c_required_by(C, FD), c_status(C, 'FALSE').
-% fd_realisability(FD, true):- fd(FD). % TODO: infer realisability OK
+
+fd_realisability(FD, false) :-
+  \+triple(FD, tomasys:'fd_realisability', _),
+  c_required_by(C, FD), c_status(C, 'FALSE').
+
+fd_realisability(FD, true):-
+  fd(FD), \+triple(FD, tomasys:'fd_realisability', _),
+  \+fd_realisability(FD, false).
