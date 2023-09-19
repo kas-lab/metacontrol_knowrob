6caeaa09e97492f69b2b607ebe35c595dce8d5ef
Gustavo <g.rezendesilva@tudelft.nl>
2023-09-19 14:34:40 +0200

    ✅ fixture run only once, add tests

--- test/rules/test_rules.py
+++ test/rules/test_rules.py
@@ -5,15 +5,10 @@ import rospy
 from metacontrol_knowrob.prolog_query import PrologQuery
 
 
-@pytest.fixture
-def node():
-    rospy.init_node('test_rules', anonymous=True)
-
-
-@pytest.fixture
+@pytest.fixture(scope="module")
 def prolog_query():
+    rospy.init_node('test_rules', anonymous=True)
     pq = PrologQuery()
-
     query = pq.query(
         "load_owl('package://metacontrol_knowrob/test/owl/test.owl',\
          [namespace(metacontrol_test, \
@@ -33,7 +28,7 @@ def evaluate_query_results(results, expected_results):
     return False
 
 
-def test_objective_in_error(node, prolog_query):
+def test_objective_in_error(prolog_query):
     results = prolog_query.query(
         "objective_status(O, S).", print_solutions=True)
     expected_results = [
@@ -47,7 +42,7 @@ def test_objective_in_error(node, prolog_query):
     assert evaluate_query_results(results, expected_results)
 
 
-def test_objective_status(node, prolog_query):
+def test_objective_status(prolog_query):
     results = prolog_query.query(
         "objective_status(O, S).", print_solutions=True)
     expected_results = [
@@ -61,8 +56,9 @@ def test_objective_status(node, prolog_query):
     assert evaluate_query_results(results, expected_results)
 
 
-# TODO: check if there are extra wrong values, e.g., ('fg_less_operator_error', 'OK')
-def test_fg_status(node, prolog_query):
+# TODO: check if there are extra wrong values,
+# e.g., ('fg_less_operator_error', 'OK')
+def test_fg_status(prolog_query):
     results = prolog_query.query("fg_status(FG, S).", print_solutions=True)
     expected_results = [
         ('fg_fake_in_error_nfr', 'IN_ERROR_NFR'),
@@ -74,13 +70,25 @@ def test_fg_status(node, prolog_query):
     assert evaluate_query_results(results, expected_results)
 
 
-def test_fd_realisability(node, prolog_query):
+def test_fd_realisability(prolog_query):
     results = prolog_query.query(
-        "fd_realisability(FD, S).", print_solutions=True)
+        "fd_realisability(FD, S)", print_solutions=True)
     expected_results = [
         ('fd_fake_component', False),
         ('fd_fake1', True),
-        ('fd_fake_critical', True),
-        ('fd_fake_less_operator', True),
+        ('fd_fake_critical', False),
+        ('fd_fake_less_operator', False),
+    ]
+    assert evaluate_query_results(results, expected_results)
+
+
+def test_fg_with_estimated_qa_type(prolog_query):
+    results = prolog_query.query(
+        "fg_with_estimated_qa_type(FG, QAT).", print_solutions=True)
+    expected_results = [
+        ('fg_fake_critical', 'qa_critical'),
+        ('fg_fake_in_error_nfr', 'mockiness'),
+        ('fg_fake_in_error_nfr', 'performance'),
+        ('fg_less_operator_error', 'qa_less_operator'),
     ]
     assert evaluate_query_results(results, expected_results)
