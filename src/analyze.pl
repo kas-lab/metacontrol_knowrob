9e3584d4d14173515c62691bcc8ed553eb136287
Gustavo <g.rezendesilva@tudelft.nl>
2023-09-19 14:33:00 +0200

    ✨ check fd_realizability for QA violations

--- src/analyze.pl
+++ src/analyze.pl
@@ -12,7 +12,9 @@
       fg_solves_obj/2,
       fg_type/2,
       fg_measured_qa/2,
+      fg_measured_qa_violation/2,
       fd_estimated_qa/2,
+      fg_with_estimated_qa_type/2,
       qa_is_type/2,
       qa_has_value/2,
       qa_comparison_operator/2,
@@ -66,8 +68,6 @@ objective_status(O, S) :-
   fg_solves_obj(FG, O), fg_status(FG, S).
 objective_status(O, 'UNGROUNDED') :-
   objective(O), \+triple(O, tomasys:'o_status', _), \+fg_solves_obj(_, O).
-% TODO: infer objective UNGROUNDED
-% TODO: infer objective OK/IN_PROGRESS
 % TODO: infer objective status when objective has a required QA
 
 objective_in_error(O) :- objective_status(O, S), S \= 'OK'.
@@ -85,6 +85,9 @@ fg_measured_qa_violation(FG, QAT) :-
   fg_measured_qa(FG, MQA), fg_type(FG, FD), fd_estimated_qa(FD, EQA),
   qa_is_type(MQA, QAT), qa_is_type(EQA, QAT), qa_has_value(MQA, MV),
   qa_has_value(EQA, EV), qa_violation(QAT, MV, EV).
+
+fg_with_estimated_qa_type(FG, QAT) :-
+  fg_type(FG, FD), fd_estimated_qa(FD, EQA), qa_is_type(EQA, QAT).
 
 %% FG status
 fg_status(FG, S) :- fg(FG), triple(FG, tomasys:'fg_status', S).
@@ -113,7 +116,7 @@ fg_status(FG, 'OK') :-
   \+fg_status(FG, 'IN_ERROR_COMPONENT'),
   \+fg_status(FG, 'IN_ERROR_FR'),
   \+fg_status(FG, 'IN_ERROR_NFR'),
-  \+fg_measured_qa_violation(FG,_).
+  \+fg_measured_qa_violation(FG, _).
 
 % FD Realisability (CompErr)
 fd_realisability(FD, S):- fd(FD), triple(FD, tomasys:'fd_realisability', S).
@@ -121,6 +124,12 @@ fd_realisability(FD, S):- fd(FD), triple(FD, tomasys:'fd_realisability', S).
 fd_realisability(FD, false) :-
   \+triple(FD, tomasys:'fd_realisability', _),
   c_required_by(C, FD), c_status(C, 'FALSE').
+
+fd_realisability(FD, false) :-
+  \+triple(FD, tomasys:'fd_realisability', _),
+  fd_estimated_qa(FD, EQA), qa_is_type(EQA, QAT),
+  fg(FG), fg_measured_qa(FG, MQA), qa_is_type(MQA, QAT),
+  qa_has_value(MQA, MV), qa_has_value(EQA, EV), qa_violation(QAT, MV, EV).
 
 fd_realisability(FD, true):-
   fd(FD), \+triple(FD, tomasys:'fd_realisability', _),
